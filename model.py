f7292c385986ea6060ab05bf18235fdf9d4ddf9d
Christian <33289198+cman2000@users.noreply.github.com>
2017-12-04 23:16:57 +0000

    Practical 2 containers
    All chnages needed for practical 2, includes notes

--- model.py
+++ model.py
@@ -5,72 +5,86 @@ Spyder Editor
 This is a temporary script file.
 """
 import random #imports from random module
+import operator # function used to find max on second part to list (1)
+import matplotlib.pyplot #chart plotting
 
+agents = []#creates alist called agents
 #set up variables
-y0 = random.randint(0,99) # Make a y variable. using random number between 0 and 99
-x0 = random.randint(0,99) # Make a x variable.using random number between 0 and 99
+#y0 = random.randint(0,99) # Make a y variable. using random number between 0 and 99
+#x0 = random.randint(0,99) # Make a x variable.using random number between 0 and 99
+agents.append([random.randint(0,99),random.randint(0,99)])#appends values to list agents
+
 
 # Random walk one step on y.
 if random.random() < 0.5:
-    y0 += 1
+    agents [0] [0] += 1
 else:
-    y0 -= 1
+    agents [0] [0] -= 1
  # Random walk one step on x.   
 if random.random() < 0.5:
-    x0 += 1
+    agents [0] [1] += 1
 else:
-    x0 -= 1
+    agents [0] [1] -= 1
 #show both values on one line   
-print (y0, x0) 
+#print (agents [0] [0],agents[0] [1]) 
   
-#repeat step number twice    
+#repeatstep numbertwice    
 if random.random() < 0.5:
-    y0 += 1
+    agents [0] [0] += 1
 else:
-    y0 -= 1
+    agents [0] [0] -= 1
     
 if random.random() < 0.5:
-    x0 += 1
+    agents [0] [1] += 1
 else:
-    x0 -= 1    
+    agents [0] [1] -= 1    
     
-print (y0, x0)
-
+print (agents[0] [0],agents[0] [1])
 
 
 
 #set up variables for second agent
-y1 = 50 # Make a y variable.
-x1 = 50 # Make a x variable.
+y1 = random.randint(0,99) # Make a y variable. using random number between 0 and 99
+x1 = random.randint(0,99)# Make a y variable. using random number between 0 and 99
+agents.append([y1,x1])# add to list
 
 # Random walk one step on y.
 if random.random() < 0.5:
-    y1 += 1
+    agents [1] [0] += 1
 else:
-    y1 -= 1
- # Random walk one step on y.   
+    agents [1] [0] -= 1
+ # Random walk one step on x.   
 if random.random() < 0.5:
-    x1 += 1
+    agents [1] [1] += 1
 else:
-    x1 -= 1
+    agents [1] [1] -= 1
 #show both values on one line   
 #print (y1, x1) 
   
 #repeat step number twice    
 if random.random() < 0.5:
-    y1 += 1
+    agents [1] [0] += 1
 else:
-    y1 -= 1
+    agents [1] [0] -= 1
     
 if random.random() < 0.5:
-    x1 += 1
+    agents [1] [1] += 1
 else:
-    x1 -= 1    
+    agents [1] [1] -= 1    
     
+print (agents[1] [0],agents[1] [1])
 #print (y1, x1)
     
-answer = (((y0 - y1)**2) + ((x0 - x1)**2))**0.5 # calculate distance between agents 
-print(answer)
-    
+answer = (((y0 - y1)**2) + ((x0 - x1)**2))**0.5
+#print(answer)
+#print(max(agents))
+print(max(agents, key=operator.itemgetter(1)))
+## used to find max on second part to list (1)
 
-    
+matplotlib.pyplot.ylim(0, 99) #presents agent coordinates on scatter chart from library
+matplotlib.pyplot.xlim(0, 99)
+matplotlib.pyplot.scatter(agents[0][1],agents[0][0])
+matplotlib.pyplot.scatter(agents[1][1],agents[1][0])
+m = max(agents, key=operator.itemgetter(1))#Ensures the most eastly point is always red
+matplotlib.pyplot.scatter(m[1],m[0], color='red')
+matplotlib.pyplot.show()

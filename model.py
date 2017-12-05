20c1ce25c98a686aeaf7c671248424511772bcae
Christian <33289198+cman2000@users.noreply.github.com>
2017-12-05 16:28:01 +0000

    Practical 3 Branching and loops
    Looping, through agents and number of moves

--- model.py
+++ model.py
@@ -4,87 +4,46 @@ Spyder Editor
 
 This is a temporary script file.
 """
-import random #imports from random module
-import operator # function used to find max on second part to list (1)
-import matplotlib.pyplot #chart plotting
+import random #imports function from random module
+import operator # imports function used to find max on second part to list (1)
+import matplotlib.pyplot # imports chart plotting function
 
-agents = []#creates alist called agents
 #set up variables
+num_of_agents = 10 # creates new variable with value of 10
+num_of_iterations = 100 # creates new variable with value of 100
+
 #y0 = random.randint(0,99) # Make a y variable. using random number between 0 and 99
 #x0 = random.randint(0,99) # Make a x variable.using random number between 0 and 99
-agents.append([random.randint(0,99),random.randint(0,99)])#appends values to list agents
+agents = []#creates alist called agents
+
+for i in range(num_of_agents):
+    agents.append([random.randint(0,99),random.randint(0,99)])#appends two random coordinates values between 0 and 99 to list agents, removes need for x and y variables
 
 
-# Random walk one step on y.
-if random.random() < 0.5:
-    agents [0] [0] += 1
-else:
-    agents [0] [0] -= 1
- # Random walk one step on x.   
-if random.random() < 0.5:
-    agents [0] [1] += 1
-else:
-    agents [0] [1] -= 1
-#show both values on one line   
-#print (agents [0] [0],agents[0] [1]) 
-  
-#repeatstep numbertwice    
-if random.random() < 0.5:
-    agents [0] [0] += 1
-else:
-    agents [0] [0] -= 1
+for j in range(num_of_iterations): # moves all agents by variable ammount (nested loop)
+    for i in range(num_of_agents):#loops through and creates coordinate creation code by number of agents 
+        if random.random() < 0.5:
+            agents[i][0] = (agents[i][0] + 1) % 100 # changes the boundary limit
+        else:
+            agents[i][0] = (agents[i][0] - 1) % 100
+
+        if random.random() < 0.5:
+            agents[i][1] = (agents[i][1] + 1) % 100
+        else:
+            agents[i][1] = (agents[i][1] - 1) % 100
+    #show both values on one line   
+    #print (agents [i] [0],agents[i] [1]) 
+         
     
-if random.random() < 0.5:
-    agents [0] [1] += 1
-else:
-    agents [0] [1] -= 1    
     
-print (agents[0] [0],agents[0] [1])
-
-
-
-#set up variables for second agent
-y1 = random.randint(0,99) # Make a y variable. using random number between 0 and 99
-x1 = random.randint(0,99)# Make a y variable. using random number between 0 and 99
-agents.append([y1,x1])# add to list
-
-# Random walk one step on y.
-if random.random() < 0.5:
-    agents [1] [0] += 1
-else:
-    agents [1] [0] -= 1
- # Random walk one step on x.   
-if random.random() < 0.5:
-    agents [1] [1] += 1
-else:
-    agents [1] [1] -= 1
-#show both values on one line   
-#print (y1, x1) 
-  
-#repeat step number twice    
-if random.random() < 0.5:
-    agents [1] [0] += 1
-else:
-    agents [1] [0] -= 1
     
-if random.random() < 0.5:
-    agents [1] [1] += 1
-else:
-    agents [1] [1] -= 1    
-    
-print (agents[1] [0],agents[1] [1])
-#print (y1, x1)
-    
-answer = (((y0 - y1)**2) + ((x0 - x1)**2))**0.5
+#answer = (((y0 - y1)**2) + ((x0 - x1)**2))**0.5
 #print(answer)
 #print(max(agents))
-print(max(agents, key=operator.itemgetter(1)))
+#print(max(agents, key=operator.itemgetter(1)))
 ## used to find max on second part to list (1)
-
-matplotlib.pyplot.ylim(0, 99) #presents agent coordinates on scatter chart from library
+matplotlib.pyplot.ylim(0, 99)
 matplotlib.pyplot.xlim(0, 99)
-matplotlib.pyplot.scatter(agents[0][1],agents[0][0])
-matplotlib.pyplot.scatter(agents[1][1],agents[1][0])
-m = max(agents, key=operator.itemgetter(1))#Ensures the most eastly point is always red
-matplotlib.pyplot.scatter(m[1],m[0], color='red')
+for i in range(num_of_agents): # display all agents
+    matplotlib.pyplot.scatter(agents[i][1],agents[i][0])
 matplotlib.pyplot.show()

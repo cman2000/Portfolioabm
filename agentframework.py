ceaf54d31a17ea26e1b3b01df898358e77736cd5
Christian <33289198+cman2000@users.noreply.github.com>
2017-12-19 21:52:41 +0000

    Practical 6 I/O - model

--- agentframework.py
+++ agentframework.py
@@ -8,14 +8,20 @@ Created on Tue Dec 12 12:57:58 2017
 import random
 class Agent():
     
-     def __init__(self):
+     def __init__(self, environment):
                 self.x = random.randint(0,99)  
                 self.y = random.randint(0,99)       
-                
-                
+                self.environment = environment
+                self.store = 0                 
        
      def move(self):
                 if (random.random() < 0.5):
                         self.x = self.x + 1
                 else:
                         self.x = self.x - 1
+                    
+
+     def eat(self):
+          if self.environment[self.y][self.x] > 10:
+              self.environment[self.y][self.x] -= 10
+              self.store += 10        
--- model.py
+++ model.py
@@ -4,11 +4,25 @@ Spyder Editor
 
 This is a temporary script file.
 """ 
-import random
+import random # imports relevant libraries
 import operator
 import matplotlib.pyplot
 import agentframework
+import csv
 
+f = open('datain.txt') # opens csv file from directory
+reader = csv.reader(f, quoting=csv.QUOTE_NONNUMERIC) # reads csv and ensures any non mueric characters quoted
+environment = [] #creates empty list to hold all data
+for row in reader:	# A list of rows
+    rowlist = [] #creates empty list for rows
+    for value in row:	# A list of value
+        rowlist.append(value)  #move row values to row list  	        
+    environment.append(rowlist)   # append row lists to environment list
+#print(environment) # Floats
+matplotlib.pyplot.imshow(environment) #use library to display raster values from environment list 
+matplotlib.pyplot.show()
+f.close() # closes reader 
+        
 
 def distance_between(agent0, agent1): #new function created to call pythhagorus calc for all looped agents
     return (((agent0.x - agent1.x)**2) + ((agent0.y - agent1.y)**2))**0.5
@@ -20,17 +34,18 @@ agents = []
 
 # Make the agents.
 for i in range(num_of_agents):
-        agents.append(agentframework.Agent())
+        agents.append(agentframework.Agent(environment))
 
 # Move the agents.
+
 for j in range(num_of_iterations):
     for i in range(num_of_agents):
         agents[i].move()
-
-       
+        agents[i].eat()       
 
 matplotlib.pyplot.ylim(0, 99)
 matplotlib.pyplot.xlim(0, 99)
+matplotlib.pyplot.imshow(environment)
 for i in range(num_of_agents):
     matplotlib.pyplot.scatter(agents[i].x, agents[i].y)
 matplotlib.pyplot.show()

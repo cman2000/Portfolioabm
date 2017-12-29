11000fc675ca6c40068a7c6414434c272552a594
Christian <33289198+cman2000@users.noreply.github.com>
2017-12-29 16:26:43 +0000

    Communicating and I/O

--- agentframework.py
+++ agentframework.py
@@ -8,20 +8,38 @@ Created on Tue Dec 12 12:57:58 2017
 import random
 class Agent():
     
-     def __init__(self, environment):
+     def __init__(self,environment, agents):
                 self.x = random.randint(0,99)  
                 self.y = random.randint(0,99)       
                 self.environment = environment
-                self.store = 0                 
+                self.agents = agents
+                self.store = 0
+                #self.neighbourhood = neighbourhood                 
        
      def move(self):
                 if (random.random() < 0.5):
                         self.x = self.x + 1
                 else:
                         self.x = self.x - 1
-                    
+                if (random.random() < 0.5):
+                        self.y = self.y + 1
+                else:
+                        self.y = self.y - 1    
 
      def eat(self):
           if self.environment[self.y][self.x] > 10:
               self.environment[self.y][self.x] -= 10
               self.store += 10        
+              
+     def share_with_neighbours(self, neighbourhood):
+         for agent in self.agents:
+             dist = self.distance_between(agent)
+             if dist <= neighbourhood:
+                 sum = self.store + agent.store
+                 ave = sum /2
+                 self.store = ave
+                 agent.store=ave
+                 print("sharing " + str(dist) + " " + str(ave))
+                
+     def distance_between(self, agent):
+         return (((self.x - agent.x)**2) + ((self.y - agent.y)**2))**0.5
--- model.py
+++ model.py
@@ -9,17 +9,26 @@ import operator
 import matplotlib.pyplot
 import agentframework
 import csv
+import matplotlib.animation
+
+num_of_agents = 10
+num_of_iterations = 100
+neighbourhood = 20
+
 
 f = open('datain.txt') # opens csv file from directory
 reader = csv.reader(f, quoting=csv.QUOTE_NONNUMERIC) # reads csv and ensures any non mueric characters quoted
-environment = [] #creates empty list to hold all data
+
+environment = [] #creates empty list to hold all data for environment
+agents = [] #make list called agents
 for row in reader:	# A list of rows
     rowlist = [] #creates empty list for rows
     for value in row:	# A list of value
         rowlist.append(value)  #move row values to row list  	        
     environment.append(rowlist)   # append row lists to environment list
 #print(environment) # Floats
-matplotlib.pyplot.imshow(environment) #use library to display raster values from environment list 
+
+matplotlib.pyplot.imshow(environment) #use this library to display raster values from environment list 
 matplotlib.pyplot.show()
 f.close() # closes reader 
         
@@ -27,21 +36,20 @@ f.close() # closes reader
 def distance_between(agent0, agent1): #new function created to call pythhagorus calc for all looped agents
     return (((agent0.x - agent1.x)**2) + ((agent0.y - agent1.y)**2))**0.5
 
-  
-num_of_agents = 10
-num_of_iterations = 100
-agents = []
+
+
 
 # Make the agents.
 for i in range(num_of_agents):
-        agents.append(agentframework.Agent(environment))
+        agents.append(agentframework.Agent(environment,agents))
 
 # Move the agents.
 
 for j in range(num_of_iterations):
     for i in range(num_of_agents):
         agents[i].move()
-        agents[i].eat()       
+        agents[i].eat()
+        agents[i].share_with_neighbours(neighbourhood)
 
 matplotlib.pyplot.ylim(0, 99)
 matplotlib.pyplot.xlim(0, 99)
@@ -53,4 +61,3 @@ matplotlib.pyplot.show()
 for agent0 in agents:
     for agent1 in agents:
         distance = distance_between(agent0, agent1)
-               
